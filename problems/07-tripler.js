f73b9aa589698fcafb8c348ef93f6dcd586607bf
jnorm23 <joshua_norman11@hotmail.com>
2024-06-15 15:59:25 -0400

    Question 7

--- problems/07-tripler.js
+++ problems/07-tripler.js
@@ -5,7 +5,12 @@ array.
 */
 
 // Your code here 
-
+function tripler(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] *= 3;
+    }
+    return arr;
+}
 
 // console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
 // console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
