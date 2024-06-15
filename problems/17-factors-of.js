fc192acd6a622976582b1269b862ea3b1f2bc15c
jnorm23 <joshua_norman11@hotmail.com>
2024-06-15 16:25:38 -0400

    editing

--- problems/17-factors-of.js
+++ problems/17-factors-of.js
@@ -7,13 +7,11 @@ into num with no remainder.
 // Your code here 
 function factorsOf(num) {
     let factors = [];
-
     for (let i = 1; i <= num; i++) {
         if (num % i === 0) {
             factors.push(i);
         }
     }
-
     return factors;
 }
 
