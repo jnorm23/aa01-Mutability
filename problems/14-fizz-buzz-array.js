7509235343333dd38c327b759140c20f9a89c631
jnorm23 <joshua_norman11@hotmail.com>
2024-06-15 16:12:42 -0400

    Question 14

--- problems/14-fizz-buzz-array.js
+++ problems/14-fizz-buzz-array.js
@@ -5,7 +5,17 @@ divisible by either 3 or 5, but not both.
 */
 
 // Your code here 
+function fizzBuzz(max) {
+    let result = [];
 
+    for (let i = 1; i < max; i++) {
+        if ((i % 3 === 0 || i % 5 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
+            result.push(i);
+        }
+    }
+
+    return result;
+}
 
 // console.log(fizzBuzz(12)); // [ 3, 5, 6, 9, 10 ]
 // console.log(fizzBuzz(20)); // [ 3, 5, 6, 9, 10, 12, 18 ]
