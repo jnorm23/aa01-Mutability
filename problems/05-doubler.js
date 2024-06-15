1f3f523bca9f886b675a4437fe1608365c787b87
jnorm23 <joshua_norman11@hotmail.com>
2024-06-15 15:58:01 -0400

    Questions 4, 5 & 6

--- problems/05-doubler.js
+++ problems/05-doubler.js
@@ -4,6 +4,14 @@ array but every element of the array is multiplied by 2.
 */
 
 // Your code here 
+function doubler(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] *= 2;
+    }
+    return arr;
+}
+
+// Now array is mutated to [2, 4, 6, 8]
 
 // let nums1 = [1, 2, 3, 4];
 // let doubled1 = doubler(nums1)
